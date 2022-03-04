873b79f50eaa3d4db4c68e8e6c3736f11aeb02ea
Mohammad Jahangiry <mjahangiry75@gmail.com>
2022-03-04 20:15:23 +0330

    Implement DI(Dagger Hilt)

--- app/src/main/java/github/masterj3y/subscene/MainActivity.kt
+++ app/src/main/java/github/masterj3y/subscene/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import github.masterj3y.subscene.ui.theme.SubsceneTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ common/network/src/main/java/github/masterj3y/network/NetworkConstants.kt
@@ -0,0 +1,6 @@
+package github.masterj3y.network
+
+object NetworkConstants {
+
+    const val BASE_URL = "https://subscene.com/"
+}
