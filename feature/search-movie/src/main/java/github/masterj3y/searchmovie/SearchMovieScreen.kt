086bc0c777831174bc5c1b6ad78260e96c86f556
Mohammad Jahangiry <mjahangiry75@gmail.com>
2022-03-05 13:13:36 +0330

    Implement search movie feature base

---
+++ feature/search-movie/src/main/java/github/masterj3y/searchmovie/SearchMovieScreen.kt
@@ -0,0 +1,9 @@
+package github.masterj3y.searchmovie
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchMovieScreen() {
+    Text(text = "Search Movie")
+}
