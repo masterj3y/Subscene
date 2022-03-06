3ac57619e2708fb68d524fc0c9bb251741d9907a
Mohammad Jahangiry <mjahangiry75@gmail.com>
2022-03-07 00:31:17 +0330

    Fix search movie event test

--- feature/search-movie/src/test/java/github/masterj3y/searchmovie/SearchMovieViewModelTest.kt
+++ feature/search-movie/src/test/java/github/masterj3y/searchmovie/SearchMovieViewModelTest.kt
@@ -3,10 +3,10 @@ package github.masterj3y.searchmovie
 import github.masterj3y.searchmovie.mockdata.MockData
 import github.masterj3y.subscene.data.SubtitleRepository
 import github.masterj3y.testutils.coroutine.CoroutinesTestRule
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
@@ -34,13 +34,15 @@ class SearchMovieViewModelTest {
 
         val result = viewModel.state
             .filter {
-                it is SearchMovieState.Result
+                it is SearchMovieState.Start
             }
             .map {
-                it as SearchMovieState.Result
-            }.firstOrNull()
+                it as SearchMovieState.Start
+            }
+            .take(3)
+            .last()
 
         result shouldNotBe null
-        result?.movies?.size shouldBe 2
+        result.movies.size shouldBe 2
     }
 }
